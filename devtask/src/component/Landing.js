10835a8107ad7197be05e115dfd17bdcbc4de605
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-12-23 13:45:54 +0530

    Created landing page

---
+++ devtask/src/component/Landing.js
@@ -0,0 +1,13 @@
+import './Landing.css';
+function Landing(){
+   return(
+    <div className = "main-content">
+    <h2>Welcome to PopX</h2>
+    <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <button className="signup"> Create Account </button>
+    <button className = "login"> Already Registered? Login </button>
+    </div>
+   );
+}
+
+export default Landing;
