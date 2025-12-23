f263a8db052afba924ec7b33502a1d191fb4348b
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-12-23 21:56:55 +0530

    Created sign up page

---
+++ devtask/src/component/SignUp.js
@@ -0,0 +1,34 @@
+import './SignUp.css';
+function SignUp(){
+    return(
+        <div className = "contents">
+            <h1> Create your 
+            <br/>
+            PopX account</h1>
+            <fieldset>
+            <legend>Full Name <span className = "required">*</span></legend>
+            Mary Doe
+        </fieldset>
+        <fieldset>
+            <legend>Phone number </legend>
+            Mary Doe
+        </fieldset>
+        <fieldset>
+            <legend>Password <span className = "required">*</span></legend>
+            Mary Doe
+        </fieldset>
+        <fieldset>
+            <legend>Company name <span className = "required">*</span></legend>
+            Mary Doe
+        </fieldset>
+        <p> Are you an Agency?<span className = "required">*</span> </p>
+        <input type="radio" checked/>Yes 
+        <input type="radio"/>No
+        <br/>
+        <button> Create Account </button>
+        </div>
+    );
+
+}
+
+export default SignUp;
