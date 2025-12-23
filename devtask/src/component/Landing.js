498bf727f1f61378a9100a8139694b42f1aadde9
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-12-23 20:20:37 +0530

    Added login page link and fix css

--- devtask/src/component/Landing.js
+++ devtask/src/component/Landing.js
@@ -1,12 +1,16 @@
 import './Landing.css';
+import {Link} from 'react-router-dom';
+
 function Landing(){
    return(
     <div className = "main-content">
     <h2>Welcome to PopX</h2>
-    <p className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <p className="content">Lorem ipsum dolor sit amet, 
+    <br/>consectetur adipiscing elit.</p>
     <button className="signup"> Create Account </button>
-    <button className = "login"> Already Registered? Login </button>
+    <button className = "login"><Link to="/login"> Already Registered? Login </Link> </button>
     </div>
+   
    );
 }
 
